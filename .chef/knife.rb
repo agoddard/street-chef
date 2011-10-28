c7b76f1609200c25505de3c803b48e207ec1ed81
Anthony Goddard <anthony@anthonygoddard.com>
2011-10-28 16:15:14 -0400

    basical knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,10 @@
+log_level                :info
+log_location             STDOUT
+node_name                'workstation'
+client_key               '../private/workstation.pem'
+validation_client_name   'chef-validator'
+validation_key           '../private/validation.pem'
+chef_server_url          'http://44.44.44.10:4000'
+cache_type               'BasicFile'
+cache_options( :path => 'checksums' )
+cookbook_path     ["cookbooks"]
