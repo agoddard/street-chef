b1aeed8dea09476db13b1fbc3236c1f288301e61
Anthony Goddard <anthony@anthonygoddard.com>
2011-10-29 00:45:56 -0400

    apache 2 for site cookbooks

--- site-cookbooks/chef-init-fix/metadata.rb
+++ site-cookbooks/chef-init-fix/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures chef-init-fix"
+maintainer       "Alex Dorsk"
+maintainer_email "adorsk@whoi.edu"
+license           "Apache 2.0"
+description      "patches fixes a small bug for installs where gem binaries aren't in /usr/bin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
--- site-cookbooks/chef-validate/metadata.rb
+++ site-cookbooks/chef-validate/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures chef-validate"
+maintainer       "Anthony Goddard"
+maintainer_email "anthony@anthonygoddard.com"
+license           "Apache 2.0"
+description      "Configures keys and initial clients for street-chef"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
